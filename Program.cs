6feb1174bef69330f340d2944f68b9ec2c7a4595
Lucy Laurentino <9998329@bcs.net.nz>
2017-09-01 09:42:31 +1200

    inicial commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Comp5202_problem2_csharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           //Start the program with Clear();
+           Console.Clear();
+           
+
+            Console.WriteLine("Hello World!");
+           
+
+
+           //End the program with blank line and instructions
+           Console.ResetColor();
+           Console.WriteLine();
+           Console.WriteLine("Press <Enter> to quit the program");
+           Console.ReadKey();
+           
+        }
+    }
+}
