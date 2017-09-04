4c40300767e7a1f1685d71ca9abef1a68269f81c
Lucy Laurentino <9998329@BCS.NET.NZ>
2017-09-04 12:54:35 +1200

    finish task2

--- Program.cs
+++ Program.cs
@@ -9,24 +9,22 @@ namespace Comp5202_problem2_csharp
            //Start the program with Clear();
            Console.Clear();
            
-            var counter = 100;
-            var number = 0;
-            int i= 0;
+                   
 
-            Console.WriteLine("================================================================================");
-            Console.WriteLine("This program will display the division tables of 1 to 12 based on a number input");
-            Console.WriteLine("================================================================================");
+            Console.WriteLine("================================================================================================");
+            Console.WriteLine("This program will display the division tables of numbers less than a 100 based on a number input");
+            Console.WriteLine("================================================================================================");
             Console.WriteLine("Please type in a number less than 100");
             int input = int.Parse(Console.ReadLine());
 
-            for(i = 0; i < counter; i++);
+            
             {
-             int a = i + 1;
-             Console.WriteLine($"{a} / {12} = {a/12}");
-
+             
+             Console.WriteLine($"{input} / 12 = {input/12}");
+             
             }
-                Console.ReadLine();
-           
+            while(input < 100)
+            
 
 
            //End the program with blank line and instructions
